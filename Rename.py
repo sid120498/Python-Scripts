91277e079cb19ea8a164ba20459846f2e0175d20
sidsin <sid912005@gmail.com>
2017-12-03 09:04:44 +0530

    final commit

--- Rename.py
+++ Rename.py
@@ -8,5 +8,4 @@ for folderName, subfolders, filenames in os.walk(k):
     for filename in filenames:
         z = convert.search(filename)
         if z!=None:
-            print(z.group(2))
             shutil.move(os.path.join(folderName,filename),os.path.join(folderName,z.group(2)))
